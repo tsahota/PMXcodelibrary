dd5e2dc368c631b4a1fdea1e0f716173a6db5d77
Tarjinder Sahota <klgk669@semldxzilker.seml.astrazeneca.net>
2017-06-01 23:49:50 +0200

    removed auto email

--- R/nm.log.R
+++ R/nm.log.R
@@ -12,11 +12,11 @@ source("Scripts/gof1.R")
 ########################################
 ## main script here
 
-m1 <- nm("qpsn -m -c auto -t 3000 -- execute run1.mod -dir=1")
+m1 <- nm("qpsn -c auto -t 3000 -- execute run1.mod -dir=1")
 run(m1)
 gof1(m1$run.no)
 
-m2 <- nm("qpsn -m -c auto -t 3000 -- execute run2.mod -dir=2")
+m2 <- nm("qpsn -c auto -t 3000 -- execute run2.mod -dir=2")
 run(m2)
 gof1(m2$run.no)
 
